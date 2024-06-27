3ba67a51fdb1053bb73e951a98d374e77de3ccf6
Pajjaphon Whanchid <guyguy5050@gmail.com>
2024-06-27 07:02:26 +0700

    Update: Cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   retries: 3,
   e2e: {
-    baseUrl: "http://192.168.192.3/",
+    baseUrl: "http://192.168.192.3:3001/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
